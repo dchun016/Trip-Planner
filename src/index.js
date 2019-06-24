fcfcd76198f6992173025164096cea1f2677cb49
Sarah Ahmad <CoffeeJavaBeans@sarahmahmad.com>
2019-06-24 10:53:34 -0500

    adds map

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-const mapboxgl = require("mapbox-gl");
+const mapboxgl = require('mapbox-gl');
 
-mapboxgl.accessToken = "YOUR API TOKEN HERE";
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiY29mZmVlamF2YWJlYW5zIiwiYSI6ImNqeGFqejExeDE3NWMzdHFic3oya3dlNmsifQ.CjxZMQG9X1x1Eb9DoYc9Yg';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
+});
